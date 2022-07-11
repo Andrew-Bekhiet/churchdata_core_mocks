e28c954aa6702c7b08336fbfd8d84d8960abeb47
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-07-11 13:57:05 +0200

    refactor: storage mocks

--- lib/fakes/mock_storage_reference.dart
+++ lib/fakes/mock_storage_reference.dart
@@ -1,12 +1,22 @@
 // ignore_for_file: implementation_imports
 
+import 'package:churchdata_core/churchdata_core.dart';
 import 'package:firebase_storage_mocks/firebase_storage_mocks.dart' as s;
-import 'package:firebase_storage_mocks/src/mock_storage_reference.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'mock_storage_reference.mocks.dart';
+
+@GenerateMocks([Reference])
 class MockFirebaseStorage extends s.MockFirebaseStorage {
   @override
   MockReference ref([String? path]) {
     path ??= '/';
-    return MockReference(this, path);
+
+    final mockReference = MockReference();
+    when(mockReference.fullPath).thenReturn(path);
+    when(mockReference.name).thenReturn(path.split('/').last);
+
+    return mockReference;
   }
 }
